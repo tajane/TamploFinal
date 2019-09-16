2da2fb754acb068b2e9d285e02927d6aaddfef41
NiTiN <NiTiN@DESKTOP-727A0UM>
2019-09-16 22:10:07 +0530

    comiit to branch

---
+++ tamploautomationproject/src/test/java/com/tamplo/ityss/actionpagetestcases/Testww.java
@@ -0,0 +1,10 @@
+package com.tamplo.ityss.actionpagetestcases;
+
+public class Testww {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println();
+	}
+
+}
